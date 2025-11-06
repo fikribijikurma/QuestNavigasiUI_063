87357eb9c3d4fbc7884e18c6ef1d3b4b43b5e5b7
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-06 13:07:33 +0700

    Buat kerangka Composable untuk FormIsian

---
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -0,0 +1,11 @@
+package com.example.questnavigasiui.view
+
+
+
+@Composable
+fun Formisian(
+    jenisK: List<String> = listOf(R.string.laki_laki, R.string.perempuan),
+    onSubmitButtonClick: () -> Unit
+){
+
+}
