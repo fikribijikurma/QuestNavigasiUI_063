3f0640efd308bc815e4ca7ac404bf741c1990b5d
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-06 13:53:26 +0700

    Tambahkan data statis untuk pratinjau TampilData

--- app/src/main/java/com/example/questnavigasiui/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui/view/TampilData.kt
@@ -1,8 +1,16 @@
 package com.example.questnavigasiui.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun TampilData(
     onBackBtnClick: () -> Unit
 ) {
+    val items = listOf(
+        pair(stringResource(id = R.string.nama_lengkap),"Contoh Name"),
+        pair(stringResource(id = R.string.jenis_kelamin),"Lainnya"),
+        pair(stringResource(id = ALAMAT),"Yogyakarta"),
+    )
+
+}
