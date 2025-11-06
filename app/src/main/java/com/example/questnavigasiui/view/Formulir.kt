c5258ebca74aed3e2d53ad584676fd4c8e3325e7
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-06 13:28:54 +0700

    Tambahkan layout dasar untuk konten formulir

--- app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -1,10 +1,15 @@
 package com.example.questnavigasiui.view
 
-import android.R
+
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import com.example.questnavigasiui.R
 
 
 @Composable
@@ -12,12 +17,25 @@ fun Formisian(
     jenisK: List<String> = listOf("laki_laki", "perempuan"),
     onSubmitButtonClick: () -> Unit
 ){
-    Scaffold (modifier = Modifier,
+    Scaffold(
         topBar = {
-            title = { Text(text = stringResource(id=R.string.home),
-                color = Color.White) },
-            colors = TopAppBarDefaults.TopAppBarColors
-            (containerColor = colorResource(id = R.color.teal_700))
-    )}
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.home),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    ) { isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+    }
 
 }
