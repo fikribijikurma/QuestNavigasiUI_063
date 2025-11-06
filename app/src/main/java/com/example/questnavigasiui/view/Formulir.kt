3690421942fbdac16ea74da9b3bc0bc58df847e6
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-06 13:19:21 +0700

    Tambahkan TopAppBar pada layar FormIsian

--- app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui/view/Formulir.kt
@@ -1,11 +1,23 @@
 package com.example.questnavigasiui.view
 
+import android.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 
 @Composable
 fun Formisian(
-    jenisK: List<String> = listOf(R.string.laki_laki, R.string.perempuan),
+    jenisK: List<String> = listOf("laki_laki", "perempuan"),
     onSubmitButtonClick: () -> Unit
 ){
+    Scaffold (modifier = Modifier,
+        topBar = {
+            title = { Text(text = stringResource(id=R.string.home),
+                color = Color.White) },
+            colors = TopAppBarDefaults.TopAppBarColors
+            (containerColor = colorResource(id = R.color.teal_700))
+    )}
 
 }
